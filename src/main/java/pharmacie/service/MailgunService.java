261ff01c053544820220d2feaf0aaf6d04b81579
hamadwiem <hamadwiem@gmail.com>
2026-03-07 02:19:54 +0100

    fix data.sql execution

--- src/main/java/pharmacie/service/MailgunService.java
+++ src/main/java/pharmacie/service/MailgunService.java
@@ -1,20 +1,24 @@
 package pharmacie.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import kong.unirest.Unirest;
 
 @Service
 public class MailgunService {
 
-    private static final String API_KEY = "TA_CLE_MAILGUN";
-    private static final String DOMAIN = "sandboxxxxx.mailgun.org";
+    @Value("${mailgun.api.key}")
+    private String apiKey;
+
+    @Value("${mailgun.domain}")
+    private String domain;
 
     public void sendEmail(String to, String subject, String text) {
 
-        Unirest.post("https://api.mailgun.net/v3/" + DOMAIN + "/messages")
-                .basicAuth("api", API_KEY)
-                .field("from", "Pharmacie <mailgun@" + DOMAIN + ">")
+        Unirest.post("https://api.mailgun.net/v3/" + domain + "/messages")
+                .basicAuth("api", apiKey)
+                .field("from", "Pharmacie <mailgun@" + domain + ">")
                 .field("to", to)
                 .field("subject", subject)
                 .field("text", text)
